27020b66c43241e3bae815c28578e11b5adb5e34
Mayank Kalbhor <kalbhor@genius-germany.de>
2026-09-13 18:22:38 +0530

    fix(storefront): use standard next.config.mjs instead of ts and clean optionalDependencies

---
+++ storefront/next.config.mjs
@@ -0,0 +1,35 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./src/site/i18n/request.ts');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+    output: 'standalone',
+    cacheComponents: true,
+
+    images: {
+        // Allow SVG images
+        dangerouslyAllowSVG: true,
+        contentDispositionType: 'attachment',
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+        // This is necessary to display images from your local Vendure instance
+        dangerouslyAllowLocalIP: true,
+        remotePatterns: [
+            {
+                hostname: 'readonlydemo.vendure.io',
+            },
+            {
+                hostname: 'demo.vendure.io'
+            },
+            {
+                hostname: 'localhost'
+            },
+            {
+                hostname: 'res.cloudinary.com'
+            }
+        ],
+    }
+};
+
+export default withNextIntl(nextConfig);
